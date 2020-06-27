33884d0c9a3c600cde15730715f696651a94414b
朱骏杰 <ories.zhu@mochain.vip>
2020-06-28 04:08:03 +0800

    自己手动去获取json

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ const express = require('express')
 const app = express()
 
 app.use((request, response, next)=>{
+  console.log('request.body')
+  console.log(request.body)
   response.send('hi')
+  request.on('data',(chunk)=>{
+    console.log(chunk.toString())
+  })
   next()
 })
 
