43aebf5519ec724cb911f0fe82fe2d05c4354025
朱骏杰 <ories.zhu@mochain.vip>
2020-06-28 04:18:53 +0800

    express.static的使用

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ const express = require('express')
 const app = express()
 
 app.use(express.json())
-
+app.use(express.static('yyy'))
 
 app.use((request, response, next)=>{
   console.log('request.body')
   console.log(request.body)
   console.log(typeof request.body)
+  response.send('hi')
   next()
 })
 
