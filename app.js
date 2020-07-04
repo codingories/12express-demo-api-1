72578d595224263ec00e2e474a85de529e8b437d
朱骏杰 <ories.zhu@mochain.vip>
2020-07-04 21:34:57 +0800

    完成ejs和变量传递

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 const express = require('express')
 const app = express()
+const fn1 = require('./fn1')
+
+
 
 app.use(express.json())
 app.use(express.static('yyy'))
 app.use(express.urlencoded())
 
-app.use((request, response, next)=>{
-  console.log('request.body')
-  console.log(request.body)
-  console.log(typeof request.body)
-  response.send('hi')
-  next()
-})
+app.set('views', 'ories')
+app.set('view engine', 'ejs')
+app.locals.title = '我的个人网站'
+
+app.use(fn1)
+
+// app.use((request, response, next)=>{
+//   console.log('request.body')
+//   console.log(request.body)
+//   console.log(typeof request.body)
+//   response.send('hi')
+//   next()
+// })
 
 app.listen(3000,()=>{
   console.log('listen 成功')
