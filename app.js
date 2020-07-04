95118951e8f424c4892b9cc5696fd32d12f17950
朱骏杰 <ories.zhu@mochain.vip>
2020-07-04 22:14:11 +0800

    request.acceptsLanguages使用判断请求的语言

--- app.js
+++ app.js
@@ -8,8 +8,10 @@ app.set('views', 'ories')
 app.set('view engine', 'ejs')
 
 app.get('/users/:id',(request, response, next)=>{
-  console.log('request.xhr')
-  console.log(request.xhr)
+  console.log('request.param')
+  console.log(request.param)
+  console.log('request.acceptsLanguages')
+  console.log(request.acceptsLanguages('JP'))
   response.send('hi')
   next()
 })
