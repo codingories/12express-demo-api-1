23bfe14dd15538957728a488f3f208c26abcfc39
朱骏杰 <ories.zhu@mochain.vip>
2020-07-04 21:49:17 +0800

    演示req.params的使用，只包含路由有名字的部分

--- app.js
+++ app.js
@@ -4,23 +4,16 @@ const fn1 = require('./fn1')
 
 
 
-app.use(express.json())
-app.use(express.static('yyy'))
-app.use(express.urlencoded())
-
 app.set('views', 'ories')
 app.set('view engine', 'ejs')
-app.locals.title = '我的个人网站'
 
-app.use(fn1)
+app.get('/users/:id',(request, response, next)=>{
+  console.log('request.params')
+  console.log(request.params)
+  response.send('hi')
+  next()
+})
 
-// app.use((request, response, next)=>{
-//   console.log('request.body')
-//   console.log(request.body)
-//   console.log(typeof request.body)
-//   response.send('hi')
-//   next()
-// })
 
 app.listen(3000,()=>{
   console.log('listen 成功')
